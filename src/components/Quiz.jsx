f77c1f94df58a712fcf72ec84cdc2a85021e4667
zrh_linux <11912614@mail.sustech.edu.cn>
2025-03-19 15:38:31 -0500

    finish 202

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,11 +4,9 @@ import quizCompleteImg from "../assets/quiz-complete.png";
 import Question from "./Question.jsx";
 
 export default function Quiz() {
-  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex =
-    answerState === "" ? userAnswers.length : userAnswers.length - 1;
+  const activeQuestionIndex = userAnswers.length;
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
   if (quizIsComplete) {
@@ -20,28 +18,11 @@ export default function Quiz() {
     );
   }
 
-  const handleSelectAnswer = useCallback(
-    function handleSelectAnswer(answer) {
-      setAnswerState("answered");
-
-      setUserAnswers((preUserAnswers) => {
-        return [...preUserAnswers, answer];
-      });
-
-      setTimeout(() => {
-        if (answer === QUESTIONS[activeQuestionIndex].answers[0]) {
-          setAnswerState("correct");
-        } else {
-          setAnswerState("wrong");
-        }
-
-        setTimeout(() => {
-          setAnswerState("");
-        }, 500);
-      }, 500);
-    },
-    [activeQuestionIndex]
-  );
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(answer) {
+    setUserAnswers((preUserAnswers) => {
+      return [...preUserAnswers, answer];
+    });
+  }, []);
 
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
@@ -52,11 +33,8 @@ export default function Quiz() {
     <div id="quiz">
       <Question
         key={activeQuestionIndex}
-        questionText={QUESTIONS[activeQuestionIndex].text}
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        answerState={answerState}
+        index={activeQuestionIndex}
         onSelectAnswer={handleSelectAnswer}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
         onSkipAnswer={handleSkipAnswer}
       ></Question>
     </div>
